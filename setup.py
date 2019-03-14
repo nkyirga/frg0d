bfec2d835f0b2b3fb18f1e824946e39d969ab99c
Nahom Yirga <kiflenahom@gmail.com>
2019-03-13 22:05:30 -0400

    fRG 0D Package Initialization

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='frg0d',
+      version='0.1',
+      description='FRG solver for impurity Hamiltonians',
+      author='Nahom Yirga',
+      author_email='nkyirga@bu.edu',
+      packages=['frg0d'],
+      zip_safe=False)
